4002b7a88058ac340b6076f767db7f2afaa00390
Oleksandr Solokha <oleksandrsolokha@MacBook-Air-Oleksandr.local>
2020-02-28 21:48:28 +0200

    Start my project. Ok) Create Bar Item Controller and Add view Controller. Add two button to Add view Controller "Add" and "Cancel"

---
+++ InternetShopKeeper/AddItemViewController.swift
@@ -0,0 +1,41 @@
+//
+//  AddItemViewController.swift
+//  InternetShopKeeper
+//
+//  Created by Oleksandr Solokha on 28.02.20.
+//  Copyright © 2020 Oleksandr Solokha. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func AddButtonAction(_ sender: UIButton) {
+    }
+    @IBAction func CancelButtonAction(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
