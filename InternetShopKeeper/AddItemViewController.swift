2889794f249f6ae87971d052fd5fd258917e0d68
Oleksandr Solokha <oleksandrsolokha@MacBook-Air-Oleksandr.local>
2020-02-29 22:23:16 +0200

    Add function to addImage Button - choose photo from gallery or take photo from camera of device

--- InternetShopKeeper/AddItemViewController.swift
+++ InternetShopKeeper/AddItemViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class AddItemViewController: UIViewController {
-
+class AddItemViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+   
     
-    
+    @IBOutlet weak var addImage: UIImageView!
+    var imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,11 +24,52 @@ class AddItemViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    @IBAction func imageButtonAction(_ sender: UIButton) {
+    @IBAction func addImageButtonAction(_ sender: UIButton) {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Зробити фото", style: .default, handler: { _ in
+            self.openCamera()
+        }))
         
+        alert.addAction(UIAlertAction(title: "Вибрати з галереї", style: .default, handler: { _ in
+            self.openGallary()
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Скасувати", style: .cancel, handler: nil))
+        
+        //If you want work actionsheet on ipad then you have to use popoverPresentationController to present the actionsheet, otherwise app will crash in iPad
+//        switch UIDevice.current.userInterfaceIdiom {
+//        case .pad:
+//            alert.popoverPresentationController?.sourceView = sender
+//            alert.popoverPresentationController?.sourceRect = sender.bounds
+//            alert.popoverPresentationController?.permittedArrowDirections = .up
+//        default:
+//            break
+//        }
+        
+        self.present(alert, animated: true, completion: nil)
+        sender.isHidden = true
     }
-    
-    
+    func openCamera(){
+        if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerControllerSourceType.camera)){
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+            imagePicker.allowsEditing = true
+            imagePicker.delegate = self
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        else{
+            let alert  = UIAlertController(title: "Помилка", message: "У Вас немає камери", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    func openGallary(){
+        imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+   
     
     @IBAction func AddButtonAction(_ sender: UIButton) {
         print("Press ADD.")
@@ -38,14 +80,23 @@ class AddItemViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        /*
+         Get the image from the info dictionary.
+         If no need to edit the photo, use `UIImagePickerControllerOriginalImage`
+         instead of `UIImagePickerControllerEditedImage`
+         */
+        if let editedImage = info[UIImagePickerControllerEditedImage] as? UIImage{
+            self.addImage.image = editedImage
+        }
+        
+        //Dismiss the UIImagePicker after selection
+        picker.dismiss(animated: true, completion: nil)
     }
-    */
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.isNavigationBarHidden = false
+        self.dismiss(animated: true, completion: nil)
+    }
 
 }
