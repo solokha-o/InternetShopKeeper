01e4ed5fe8872a5f92d4ba67d4bc856b38b9b1db
Oleksandr Solokha <oleksandr.solokha@gmail.com>
2020-04-06 22:10:19 +0300

    fixed LaunchScreen

---
+++ InternetShopKeeper/LaunchScreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LaunchScreenViewController.swift
+//  InternetShopKeeper
+//
+//  Created by Oleksandr Solokha on 06.04.2020.
+//  Copyright © 2020 Oleksandr Solokha. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
