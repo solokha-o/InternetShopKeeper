26428242b0d49aa063fd324910e49b0f94c31e50
Oleksandr Solokha <oleksandr.solokha@gmail.com>
2020-04-08 21:23:54 +0300

    /

---
+++ InternetShopKeeper/ModelFirstLaunch.swift
@@ -0,0 +1,16 @@
+//
+//  modelFirstLaunch.swift
+//  InternetShopKeeper
+//
+//  Created by Oleksandr Solokha on 08.04.2020.
+//  Copyright © 2020 Oleksandr Solokha. All rights reserved.
+//
+
+import Foundation
+
+// model for view in onboarding
+struct ModelFirstLaunch {
+    let imageName : String
+    let title : String
+    let subtitle : String
+}
