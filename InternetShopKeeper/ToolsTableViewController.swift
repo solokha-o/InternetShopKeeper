5732df4cdb4e693d8703d87411ee63749e7b94a3
Oleksandr Solokha <oleksandr.solokha@gmail.com>
2020-04-13 17:04:33 +0300

    configure cell in  ToolsTableViewController configure select row create MFMailComposeViewController Configure the fields of the interface Present the view controller modally if mail on device is not available extention for mail message

--- InternetShopKeeper/ToolsTableViewController.swift
+++ InternetShopKeeper/ToolsTableViewController.swift
@@ -7,79 +7,77 @@
 //
 
 import UIKit
+import MessageUI
 
 class ToolsTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.prefersLargeTitles = true
+        tableView.tableFooterView = UIView()
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 2
     }
 
-    /*
+    // configure cell
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToolCell", for: indexPath)
+        
+        switch indexPath.row {
+        case 0:
+            cell.textLabel?.text = "Написати нам!"
+            cell.detailTextLabel?.text = "Задати питання через e-mail"
+        case 1:
+            let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+            cell.textLabel?.text = "Версія додатку:"
+            cell.detailTextLabel?.text = appVersion
+            cell.accessoryType = .none
+        default:
+            break
+        }
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    //configure select row
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 0:
+            //create MFMailComposeViewController
+            let composerVC = MFMailComposeViewController()
+            if MFMailComposeViewController.canSendMail() {
+                composerVC.mailComposeDelegate = self
+                // Configure the fields of the interface.
+                composerVC.setToRecipients(["oleksandr.solokha@gmail.com"])
+                composerVC.setSubject("")
+                composerVC.setMessageBody("", isHTML: true)
+                // Present the view controller modally
+                self.present(composerVC, animated: true, completion: nil)
+            } else {
+                // if mail on device is not available
+                print("Mail services are not available")
+                let alert = UIAlertController(title: "Помилка!", message: "У Вас відсутній додаток  Mail! Відправте пошту на адресу: oleksandr.solokha@gmail.com.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+        default:
+            break
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+}
+// extention for mail message
+extension ToolsTableViewController: MFMailComposeViewControllerDelegate {
+    private func mailComposeController(controller: MFMailComposeViewController,
+                               didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+        // Dismiss the mail compose view controller.
+        controller.dismiss(animated: true, completion: nil)
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
